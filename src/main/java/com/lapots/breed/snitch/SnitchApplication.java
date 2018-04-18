910beda9e75ad7f4487496f01724660680ebb392
lapots <sebastrident@gmail.com>
2018-04-18 20:56:36 +0300

    Application template

---
+++ src/main/java/com/lapots/breed/snitch/SnitchApplication.java
@@ -0,0 +1,20 @@
+package com.lapots.breed.snitch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main application.
+ */
+@SpringBootApplication
+public class SnitchApplication {
+
+    /**
+     * Main application.
+     * @param args console args
+     */
+    public static void main(final String[] args) {
+        SpringApplication.run(SnitchApplication.class, args);
+    }
+
+}
