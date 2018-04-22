0ddb73bcdb7c1592e81cec91fb367ef503aba646
lapots <sebastrident@gmail.com>
2018-04-22 13:57:20 +0300

    Added REST client

--- src/main/java/com/lapots/breed/snitch/SnitchRuleCreationHandler.java
+++ src/main/java/com/lapots/breed/snitch/SnitchRuleCreationHandler.java
@@ -8,6 +8,11 @@ import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.GetObjectRequest;
 import com.amazonaws.services.s3.model.S3Object;
 import com.amazonaws.util.IOUtils;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -20,6 +25,8 @@ import java.io.InputStream;
 public class SnitchRuleCreationHandler implements RequestHandler<S3Event, String> {
     private static final Logger LOGGER = LoggerFactory.getLogger(SnitchRuleCreationHandler.class);
 
+    private static final String AWS_JUDGE_ADD_URL = System.getenv("aws.judge.url");
+
     @Override
     public String handleRequest(S3Event input, Context context) {
         AmazonS3 s3client = AmazonS3ClientBuilder.defaultClient();
@@ -31,14 +38,37 @@ public class SnitchRuleCreationHandler implements RequestHandler<S3Event, String
             LOGGER.info("Object was created: key={}, bucket={}", s3key, s3bucket);
 
             S3Object object = s3client.getObject(new GetObjectRequest(s3bucket, s3key));
-            InputStream objectData = object.getObjectContent();
-            try {
+            try (InputStream objectData = object.getObjectContent()) {
                 String uploadedRule = IOUtils.toString(objectData);
-                objectData.close();
+                callRest(uploadedRule);
             } catch (IOException e) {
                 LOGGER.error("Failed to close object stream!", e);
             }
         });
         return null;
+    }
+
+    /**
+     * Does REST call to Judge Rest API.
+     *
+     * @param json rule json
+     */
+    private void callRest(String json) {
+        try (CloseableHttpClient client = HttpClients.createDefault()) {
+            HttpPost request = new HttpPost(AWS_JUDGE_ADD_URL);
+            StringEntity entity = new StringEntity(json);
+
+            request.setEntity(entity);
+            request.setHeader("Accept", "application/json");
+            request.setHeader("Content-Type", "application/json");
+
+            CloseableHttpResponse response = client.execute(request);
+            int status = response.getStatusLine().getStatusCode();
+            if (status != 200) {
+                LOGGER.warn("Failed to do REST request. Returned code: [{}].", status);
+            }
+        } catch (IOException e) {
+            LOGGER.error("Failed to do REST call!", e);
+        }
     }
 }
