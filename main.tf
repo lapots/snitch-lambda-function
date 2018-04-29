854e12a7d084044ee3690582efd68bbf5b1b1cac
lapots <sebastrident@gmail.com>
2018-04-29 22:26:50 +0300

    Updated lambda to handle only Put events

--- main.tf
+++ main.tf
@@ -73,6 +73,6 @@ resource "aws_s3_bucket_notification" "bucket_notification" {
   bucket = "${aws_s3_bucket.game_rules.id}"
   lambda_function {
     lambda_function_arn = "${aws_lambda_function.snitch.arn}"
-    events = ["s3:ObjectCreated:*"]
+    events = ["s3:ObjectCreated:Put"]
   }
 }
